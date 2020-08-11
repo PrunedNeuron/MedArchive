054ec387212dddf0050c1687c23faaa02ff1c81f
Ayush Mishra <ayushm017@gmail.com>
2020-08-11 20:56:01 +0530

    added deletion functionality

--- src/main/java/dev/ayushm/med/Application.java
+++ src/main/java/dev/ayushm/med/Application.java
@@ -10,4 +10,9 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }*/
+
 }
